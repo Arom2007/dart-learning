64f6b63ff15ed2e8dc1d099d8504726e15cfd7c6
Arom2007 <arompradhan@gmail.com>
2025-12-17 17:24:19 +0545

    Learned about super in dart

--- dart_application_1/lib/OOP_3.dart
+++ dart_application_1/lib/OOP_3.dart
@@ -43,16 +43,69 @@ class Student extends Person {
 // Multiple Inheritance: One class has multiple parent classes. It is not supported by dart.
 
 
-// Inheritance of Constructor in Dart
+// Inheritance of Constructors in Dart
+class Laptop {
+  String? name;
+  String? color; 
+  // Constructor 
+  Laptop(this.name, this.color) {
+    print("Laptop constructor");
+  }
+}
+
+class McBook extends Laptop{
+  int? year;
+  
+  //Constructor
+  McBook(String super.name, String super.color, this.year) {
+    print("Mac Book constructor");
+  }
+  
+  void display() {
+    print("Mac name is $name. Its color is $color. It was made $year years ago");
+  }
+}
 
 
+// Super in Dart
+// Super is used to refer to the parent class.
+// It is used to call the parent class's properties and methods.
+
+class Car {
+  int numberOfSeats = 4;
+}
+
+
+class SmallCar extends Car{
+  int numberOfSeats = 2;
+
+  void display() {
+    print("Number of seats in small car is $numberOfSeats");
+    print("Number of seats in car is ${super.numberOfSeats}");
+  }
+
+}
 
 
 void main() {
   Student student = Student();
   student.name = "Dumraj";
   student.age = 12;
-  student.schoolName = "Premier Int School";
+  student.schoolName = "Premier International School";
   student.schoolAddress = "Baneshwor";
   student.displayStudent();
+
+
+  print("-" * 60);
+
+
+  McBook mcbok = McBook("Samuel", "Black", 14);
+  mcbok.display();
+
+
+  print("-" * 60);
+
+
+  SmallCar smallCar = SmallCar();
+  smallCar.display();
 }
