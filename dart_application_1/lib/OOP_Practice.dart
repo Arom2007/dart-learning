81f1b6d5a6cb4f315c299c9c45f8103aae1934aa
Arom2007 <arompradhan@gmail.com>
2025-12-22 10:55:52 +0545

    Dart OOP practice part 2

--- dart_application_1/lib/OOP_Practice.dart
+++ dart_application_1/lib/OOP_Practice.dart
@@ -26,6 +26,48 @@ class GeneratePassword {
 }
 
 
+// Program to create a Laptop class with properties and print those details
+class Laptop {
+  int? id;
+  String? name;
+  int? ram;
+
+  void displayInfo() {
+    print("Laptop with id $id is named $name and has $ram GB RAM.");
+  }
+}
+
+// Program to create class House with a constructor and method to print details.
+class House {
+  int houseID;
+  String houseName;
+  int housePrice;
+
+  House(this.houseID, this.houseName, this.housePrice);
+
+  void displayInfo() {
+    print("House with house ID $houseID is named $houseName and costs Rs $housePrice.");
+  }
+}
+
+
 void main() {
   print("Password : ${GeneratePassword.generateRandomPassword(10)}");
+
+
+  print("-" * 60);
+
+
+  Laptop plapplop = Laptop();
+  plapplop.id = 80085;
+  plapplop.name = "Yudhisthira";
+  plapplop.ram = 32;
+  plapplop.displayInfo();
+
+
+  print("-" * 60);
+
+
+  House house = House(675367, "Rekha House", 6700000000);
+  house.displayInfo();
 }
